b6b8bc7c9416b1f65986b2d43c351351b9707377
Autor <civimip573@gyn5.com>
2022-03-11 21:15:54 -0400

    1ra clase

--- app/src/main/java/org/jbtc/aniapp/ui/home/HomeFragment.java
+++ app/src/main/java/org/jbtc/aniapp/ui/home/HomeFragment.java
@@ -5,24 +5,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.jbtc.aniapp.R;
 import org.jbtc.aniapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+
         View root = binding.getRoot();
+
+        binding.button.setText("OK");
+        //String texto = binding.button.getText().toString();
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Hola Mundo", Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
