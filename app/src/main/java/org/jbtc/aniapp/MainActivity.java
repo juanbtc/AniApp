2033f46b002bcb643e33939658ef13d5cd348ca6
yalucepadi <yalucepa@gmail.com>
2022-03-12 13:09:34 -0500

    pruebas de ciclo de vida en log android fragment

--- app/src/main/java/org/jbtc/aniapp/MainActivity.java
+++ app/src/main/java/org/jbtc/aniapp/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Boton de funcion correo", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
--- app/src/main/java/org/jbtc/aniapp/ui/home/HomeFragment.java
+++ app/src/main/java/org/jbtc/aniapp/ui/home/HomeFragment.java
@@ -1,6 +1,8 @@
 package org.jbtc.aniapp.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -18,9 +21,20 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.i("CiclosDeVidaF:","onAttach");
+    }
 
-    public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("CiclosDeVidaF:","onCreate");
+    }
 
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i("CiclosDeVidaF:","onCreateView");
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
@@ -28,13 +42,13 @@ public class HomeFragment extends Fragment {
 
         View root = binding.getRoot();
 
-        binding.button.setText("OK");
+        binding.button.setText(R.string.Text_Button);
         //String texto = binding.button.getText().toString();
 
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "Hola Mundo", Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), "Home fragment", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -45,8 +59,51 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.i("CiclosDeVidaF:","onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.i("CiclosDeVidaF:","onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i("CiclosDeVidaF:","onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.i("CiclosDeVidaF:","onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i("CiclosDeVidaF:","onStop");
+        super.onStop();
+    }
+
+    @Override
     public void onDestroyView() {
+        Log.i("CiclosDeVidaF:","onDestroyView");
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("CiclosDeVidaF:","onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.i("CiclosDeVidaF:","onDetach");
+        super.onDetach();
     }
 }
