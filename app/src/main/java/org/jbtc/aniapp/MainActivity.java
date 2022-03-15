1bc1200c10a1d891a5a61a81cfd692c576f7974f
Autor <johnxt32@gmail.com>
2022-03-15 00:17:05 -0400

    test

--- app/src/main/java/org/jbtc/aniapp/MainActivity.java
+++ app/src/main/java/org/jbtc/aniapp/MainActivity.java
@@ -26,12 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         Log.i("TAG", "onCreate: ");
-
-
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
