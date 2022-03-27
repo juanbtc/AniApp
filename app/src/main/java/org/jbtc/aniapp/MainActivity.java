f88eb88aa044cf84943c2e6e18dcf63254cfba9e
yalucepadi <yalucepa@gmail.com>
2022-03-27 11:58:54 -0500

    Añade fragment Pagination Anime

--- app/src/main/java/org/jbtc/aniapp/MainActivity.java
+++ app/src/main/java/org/jbtc/aniapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.gson.JsonObject;
 
 import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
@@ -18,7 +19,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import org.jbtc.aniapp.contract.AnimeService;
 import org.jbtc.aniapp.databinding.ActivityMainBinding;
+
 import org.jbtc.aniapp.model.RespuestaAnimes;
+
+
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,24 +38,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         Log.i("TAG", "onCreate: ");
-
-
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+        /*binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Boton de funcion correo", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -61,13 +60,17 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_episode,
                 R.id.nav_song,
                 R.id.nav_user)
+
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        //initRetrofit();
     }
-    private void initRetrofit() {
+
+    /*private void initRetrofit() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.aniapi.com/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -90,32 +93,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /*animeService.getAnime(100).enqueue(new Callback<RespuestaAnime>() {
-            @Override
-            public void onResponse(Call<RespuestaAnime> call, Response<RespuestaAnime> response) {
-                Log.i("TAG", "onResponse: anime: "+response.body());
-            }
+    }*/
 
-            @Override
-            public void onFailure(Call<RespuestaAnime> call, Throwable t) {
-                Log.e("TAG", "onFailure: ", t);
-            }
-        });*/
 
-        /*animeService.autenticacion("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE0MjIiLCJuYmYiOjE2NDc0NzY0NDUsImV4cCI6MTY1MDA2ODQ0NSwiaWF0IjoxNjQ3NDc2NDQ1fQ.V5Agn5pUvTcBtDHsf3a26GW5VaUl_l0jVR6GgYkQ4Xs")
-                .enqueue(new Callback<JsonObject>() {
-                    @Override
-                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                        Log.i("TAG", "onResponse: response: "+response+" body: "+response.body() );
-                    }
 
-                    @Override
-                    public void onFailure(Call<JsonObject> call, Throwable t) {
-                        Log.e("TAG", "onFailure: ", t);
-                    }
-                });*/
-
-    }
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
---
+++ app/src/main/java/org/jbtc/aniapp/component/PaginationFragment.java
@@ -0,0 +1,106 @@
+package org.jbtc.aniapp.component;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.jbtc.aniapp.R;
+import org.jbtc.aniapp.databinding.FragmentPaginationBinding;
+
+public class PaginationFragment extends Fragment {
+    private FragmentPaginationBinding binding;
+
+    private int page=0;
+    private int total_pages=0;
+
+    private OnClickCallback onClickCallback;
+    public interface OnClickCallback{
+        void onClickPagination(int page);
+    }
+
+    public void setOnClickCallback(OnClickCallback onClickCallback) {
+        this.onClickCallback = onClickCallback;
+    }
+
+    private View.OnClickListener onClickTextView = view -> {
+        TextView boton = (TextView) view;
+        int page = Integer.valueOf( boton.getText().toString() );
+        onClickCallback.onClickPagination(page);
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentPaginationBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.tvPaginationBt01.setOnClickListener(onClickTextView);
+        binding.tvPaginationBt02.setOnClickListener(onClickTextView);
+        binding.tvPaginationBt03.setOnClickListener(onClickTextView);
+        binding.tvPaginationBt04.setOnClickListener(onClickTextView);
+        binding.tvPaginationBt05.setOnClickListener(onClickTextView);
+        binding.tvPaginationBt06.setOnClickListener(onClickTextView);
+        binding.tvPaginationBt07.setOnClickListener(onClickTextView);
+        binding.tvPaginationBt08.setOnClickListener(onClickTextView);
+        binding.tvPaginationBt09.setOnClickListener(onClickTextView);
+    }
+
+    public void setEnumeration(int page,int total_pages){
+        this.page = page;
+        this.total_pages = total_pages;
+
+        int i = page-2;
+        int j = page+2;
+
+        if(i<3){
+            binding.tvPaginationBt03.setText("3");
+            binding.tvPaginationBt04.setText("4");
+            binding.tvPaginationBt05.setText("5");
+            binding.tvPaginationBt06.setText("6");
+            binding.tvPaginationBt07.setText("7");
+        }else if(2<i && j<total_pages-2){
+            binding.tvPaginationBt03.setText(String.valueOf(i));
+            binding.tvPaginationBt04.setText(String.valueOf(i+1));
+            binding.tvPaginationBt05.setText(String.valueOf(i+2));
+            binding.tvPaginationBt06.setText(String.valueOf(i+3));
+            binding.tvPaginationBt07.setText(String.valueOf(i+4));
+        }else if(i<total_pages){
+            binding.tvPaginationBt03.setText(String.valueOf(total_pages-6));
+            binding.tvPaginationBt04.setText(String.valueOf(total_pages-5));
+            binding.tvPaginationBt05.setText(String.valueOf(total_pages-4));
+            binding.tvPaginationBt06.setText(String.valueOf(total_pages-3));
+            binding.tvPaginationBt07.setText(String.valueOf(total_pages-2));
+        }
+        binding.tvPaginationBt08.setText(String.valueOf(total_pages-1));
+        binding.tvPaginationBt09.setText(String.valueOf(total_pages));
+
+        int n = binding.llPaginationContainer.getChildCount();
+        for (int l=0;l<n;l++){
+            TextView et = (TextView)binding.llPaginationContainer.getChildAt(l);
+            if(String.valueOf(page).equals(et.getText().toString())){
+                et.setBackgroundColor(getResources().getColor(R.color.teal_200));
+            }else{
+                et.setBackgroundTintList(null);
+                //et.setBackground(getResources().getDrawable(R.drawable.bg_btn_pagination));
+                //et.setBackground(getResources().getDrawable(R.drawable.bg_btn_pagination));
+            }
+        }
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding=null;
+    }
+}
